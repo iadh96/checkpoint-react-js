ff7dd38848b91f489282585607d1c573c1fc7f6f
iadh96 <iadhboussetta@gmail.com>
2021-11-09 14:20:49 +0100

    checkpoint reactjsx

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import Formone from './formulaire'
+
+function App() {
+  return(
+    <div className="App">
+      <h1>Welcome</h1>
+      
+<Formone className="Forme"/>
+
+
+  
+  </div>
+  );
+ }
+
+export default App;
